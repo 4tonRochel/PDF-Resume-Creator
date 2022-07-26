af969c38454539f0c5edd181d82df8be76f17ab6
4tonRochel <furtonrochel@gmail.com>
2022-07-26 20:22:22 +0800

    PDF Resume Creator App

---
+++ PDF Resume Creator/Form1.Designer.vb
@@ -0,0 +1,89 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.richtxbxDisplay = New System.Windows.Forms.RichTextBox()
+        Me.btnLoad = New System.Windows.Forms.Button()
+        Me.btnGenerate = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'richtxbxDisplay
+        '
+        Me.richtxbxDisplay.Font = New System.Drawing.Font("Times New Roman", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.richtxbxDisplay.Location = New System.Drawing.Point(12, 12)
+        Me.richtxbxDisplay.Name = "richtxbxDisplay"
+        Me.richtxbxDisplay.Size = New System.Drawing.Size(557, 557)
+        Me.richtxbxDisplay.TabIndex = 0
+        Me.richtxbxDisplay.Text = ""
+        '
+        'btnLoad
+        '
+        Me.btnLoad.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnLoad.Location = New System.Drawing.Point(586, 78)
+        Me.btnLoad.Name = "btnLoad"
+        Me.btnLoad.Size = New System.Drawing.Size(155, 39)
+        Me.btnLoad.TabIndex = 1
+        Me.btnLoad.Text = "Load JSON "
+        Me.btnLoad.UseVisualStyleBackColor = True
+        '
+        'btnGenerate
+        '
+        Me.btnGenerate.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnGenerate.Location = New System.Drawing.Point(586, 138)
+        Me.btnGenerate.Name = "btnGenerate"
+        Me.btnGenerate.Size = New System.Drawing.Size(155, 39)
+        Me.btnGenerate.TabIndex = 2
+        Me.btnGenerate.Text = "Generate Resume"
+        Me.btnGenerate.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(586, 530)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(155, 39)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(744, 571)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnGenerate)
+        Me.Controls.Add(Me.btnLoad)
+        Me.Controls.Add(Me.richtxbxDisplay)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents richtxbxDisplay As RichTextBox
+    Friend WithEvents btnLoad As Button
+    Friend WithEvents btnGenerate As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ PDF Resume Creator/Form1.vb
@@ -0,0 +1,166 @@
+﻿Imports iTextSharp.text.pdf.draw
+Imports iTextSharp.text.pdf
+Imports iTextSharp.text
+Imports Newtonsoft.Json
+Imports System.IO
+Public Class Form1
+    Dim Read As String = "C:\Users\SherylMejia\Documents\PDF Resume Creator\PDF Resume Creator\Resume.json"
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnLoad_Click(sender As Object, e As EventArgs) Handles btnLoad.Click
+        Dim Display As DialogResult = MessageBox.Show("Confirm if you want to load JSON File", "Resume Generator", MessageBoxButtons.OKCancel, MessageBoxIcon.Information)
+        If Display = DialogResult.OK Then
+            richtxbxDisplay.Text = File.ReadAllText(Read)
+        End If
+    End Sub
+
+    Private Sub btnGenerate_Click(sender As Object, e As EventArgs) Handles btnGenerate.Click
+        Dim Create As DialogResult = MessageBox.Show("Confirm if you want to Generate your Resume", "Resume Generator", MessageBoxButtons.OKCancel, MessageBoxIcon.Information)
+        If Create = DialogResult.OK Then
+            Dim Path As String = "C:\Users\SherylMejia\Documents\PDF Resume Creator\PDF Resume Creator\Resume.json"
+            Dim Reader As String = File.ReadAllText(Path)
+            Dim Data As Result = JsonConvert.DeserializeObject(Of Result)(Reader)
+            Dim Generate As Document = New Document()
+            PdfWriter.GetInstance(Generate, New FileStream("C:\Users\SherylMejia\Documents\PDF Resume Creator\PDF Resume Creator\FURTON_ROCHEL.pdf", FileMode.Create))
+            Dim Line As LineSeparator = New LineSeparator(3.0F, 100.0F, BaseColor.BLACK, Element.ALIGN_CENTER, 1)
+            Dim Space As New Paragraph(" ")
+            Dim F1 As iTextSharp.text.Font = FontFactory.GetFont(iTextSharp.text.Font.FontFamily.TIMES_ROMAN.ToString(), 20, iTextSharp.text.Font.BOLD)
+            Dim F2 As iTextSharp.text.Font = FontFactory.GetFont(iTextSharp.text.Font.FontFamily.TIMES_ROMAN.ToString(), 14, iTextSharp.text.Font.BOLD)
+            Dim F3 As iTextSharp.text.Font = FontFactory.GetFont(iTextSharp.text.Font.FontFamily.TIMES_ROMAN.ToString(), 13, iTextSharp.text.Font.BOLD)
+            Dim F4 As iTextSharp.text.Font = FontFactory.GetFont(iTextSharp.text.Font.FontFamily.TIMES_ROMAN.ToString(), 12, iTextSharp.text.Font.NORMAL)
+            Dim Image As iTextSharp.text.Image = iTextSharp.text.Image.GetInstance(Data.Image)
+            Image.ScalePercent(13.0F)
+            Image.SetAbsolutePosition(Generate.PageSize.Width - 30.0F - 133.0F, Generate.PageSize.Height - 30.0F - 100.0F)
+
+            Dim Name As New Paragraph(Data.FullName & vbLf, F1)
+            Dim Prof As New Paragraph(Data.Profession & vbLf, F4)
+            Dim Phone As New Paragraph(Data.Phone & vbLf, F4)
+            Dim Email As New Paragraph(Data.Email & vbLf, F4)
+            Dim H1 As New Paragraph(Data.Header1 & vbLf, F2)
+            Dim Quali As New Paragraph(Data.Qualifications & vbLf, F4)
+            Dim H2 As New Paragraph(Data.Header2 & vbLf, F2)
+            Dim Pos As New Paragraph(Data.Position & vbLf, F4)
+            Dim Com As New Paragraph(Data.Company & vbLf, F4)
+            Dim Job As New Paragraph(Data.JobDescription & vbLf, F4)
+            Dim H3 As New Paragraph(Data.Header3 & vbLf, F2)
+            Dim SH1 As New Paragraph(Data.SubHeader1 & vbLf, F3)
+            Dim S1 As New Paragraph(Data.Skill1 & vbLf, F4)
+            Dim S2 As New Paragraph(Data.Skill2 & vbLf, F4)
+            Dim S3 As New Paragraph(Data.Skill3 & vbLf, F4)
+            Dim S4 As New Paragraph(Data.Skill4 & vbLf, F4)
+            Dim SH2 As New Paragraph(Data.SubHeader2 & vbLf, F3)
+            Dim S5 As New Paragraph(Data.Skill5 & vbLf, F4)
+            Dim S6 As New Paragraph(Data.Skill6 & vbLf, F4)
+            Dim SH3 As New Paragraph(Data.SubHeader3 & vbLf, F3)
+            Dim S7 As New Paragraph(Data.Skill7 & vbLf, F4)
+            Dim S8 As New Paragraph(Data.Skill8 & vbLf, F4)
+            Dim H4 As New Paragraph(Data.Header4 & vbLf, F2)
+            Dim Course As New Paragraph(Data.Course & vbLf, F4)
+            Dim School As New Paragraph(Data.School & vbLf, F4)
+            Dim Year As New Paragraph(Data.Year & vbLf, F4)
+
+            Name.Alignment = Element.ALIGN_JUSTIFIED
+            Prof.Alignment = Element.ALIGN_JUSTIFIED
+            Phone.Alignment = Element.ALIGN_JUSTIFIED
+            Email.Alignment = Element.ALIGN_JUSTIFIED
+            H1.Alignment = Element.ALIGN_JUSTIFIED
+            Quali.Alignment = Element.ALIGN_JUSTIFIED
+            H2.Alignment = Element.ALIGN_JUSTIFIED
+            Pos.Alignment = Element.ALIGN_JUSTIFIED
+            Com.Alignment = Element.ALIGN_JUSTIFIED
+            Job.Alignment = Element.ALIGN_JUSTIFIED
+            H3.Alignment = Element.ALIGN_JUSTIFIED
+            SH1.Alignment = Element.ALIGN_JUSTIFIED
+            S1.Alignment = Element.ALIGN_JUSTIFIED
+            S2.Alignment = Element.ALIGN_JUSTIFIED
+            S3.Alignment = Element.ALIGN_JUSTIFIED
+            S4.Alignment = Element.ALIGN_JUSTIFIED
+            SH2.Alignment = Element.ALIGN_JUSTIFIED
+            S5.Alignment = Element.ALIGN_JUSTIFIED
+            S6.Alignment = Element.ALIGN_JUSTIFIED
+            SH3.Alignment = Element.ALIGN_JUSTIFIED
+            S7.Alignment = Element.ALIGN_JUSTIFIED
+            S8.Alignment = Element.ALIGN_JUSTIFIED
+            H4.Alignment = Element.ALIGN_JUSTIFIED
+            Course.Alignment = Element.ALIGN_JUSTIFIED
+            School.Alignment = Element.ALIGN_JUSTIFIED
+            Year.Alignment = Element.ALIGN_JUSTIFIED
+
+            Generate.Open()
+            Generate.Add(Image)
+            Generate.Add(Name)
+            Generate.Add(Prof)
+            Generate.Add(Phone)
+            Generate.Add(Email)
+            Generate.Add(Space)
+            Generate.Add(Line)
+            Generate.Add(H1)
+            Generate.Add(Space)
+            Generate.Add(Quali)
+            Generate.Add(Space)
+            Generate.Add(Line)
+            Generate.Add(H2)
+            Generate.Add(Space)
+            Generate.Add(Pos)
+            Generate.Add(Com)
+            Generate.Add(Job)
+            Generate.Add(Space)
+            Generate.Add(Line)
+            Generate.Add(H3)
+            Generate.Add(Space)
+            Generate.Add(SH1)
+            Generate.Add(S1)
+            Generate.Add(S2)
+            Generate.Add(S3)
+            Generate.Add(S4)
+            Generate.Add(SH2)
+            Generate.Add(S5)
+            Generate.Add(S6)
+            Generate.Add(SH3)
+            Generate.Add(S7)
+            Generate.Add(S8)
+            Generate.Add(Space)
+            Generate.Add(Line)
+            Generate.Add(H4)
+            Generate.Add(Space)
+            Generate.Add(Course)
+            Generate.Add(School)
+            Generate.Add(Year)
+            Generate.Close()
+            richtxbxDisplay.Text = " "
+            MessageBox.Show("Successfully created Resume!", "Resume Generator")
+        End If
+    End Sub
+
+    Public Class Result
+        Public Property Image As String
+        Public Property FullName As String
+        Public Property Profession As String
+        Public Property Phone As String
+        Public Property Email As String
+        Public Property Header1 As String
+        Public Property Qualifications As String
+        Public Property Header2 As String
+        Public Property Position As String
+        Public Property Company As String
+        Public Property JobDescription As String
+        Public Property Header3 As String
+        Public Property SubHeader1 As String
+        Public Property Skill1 As String
+        Public Property Skill2 As String
+        Public Property Skill3 As String
+        Public Property Skill4 As String
+        Public Property SubHeader2 As String
+        Public Property Skill5 As String
+        Public Property Skill6 As String
+        Public Property SubHeader3 As String
+        Public Property Skill7 As String
+        Public Property Skill8 As String
+        Public Property Header4 As String
+        Public Property Course As String
+        Public Property School As String
+        Public Property Year As String
+    End Class
+End Class
